1bc6a35b944015016d8bc17996b1c8d83452c971
Mike Marino <marino1820@gmail.com>
2021-01-11 21:57:37 -0500

    Added styling and javascript to buttons

--- assets/script.js
+++ assets/script.js
@@ -13,23 +13,75 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// generate
+function generatePassword() {
+
+var pw = "";
+var pwLength = "";
 var emptyString = "";
 var lowerCaseAlpha = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "1234567890";
 var specialChar ="!@#$%^&*()-_+";
 
-// add these string together heh
+var pwLength = document.querySelector("#charCount").value 
 
+var rd1 = document.getElementById("option1");
+var rd2 = document.getElementById("option2");
+var rd3 = document.getElementById("option3");
+var rd4 = document.getElementById("option4");
 
-
-while (emptyString.length < 726) {
-  emptyString += upperCaseAlpha[Math.floor(Math.random() * upperCaseAlpha.length)];
-  emptyString += lowerCaseAlpha[Math.floor(Math.random() * lowerCaseAlpha.length)];
-  emptyString += numeric[Math.floor(Math.random() * numeric.length)];
-  emptyString += specialChar[Math.floor(Math.random() * specialChar.length)];
+if (rd1.checked===true)  {
+pw = pw + lowerCaseAlpha;
 }
-console.log(emptyString);
+
+if (rd2.checked===true){
+pw = pw + upperCaseAlpha;  
+}
+
+if (rd3.checked===true){
+pw = pw + numeric;  
+}
+
+if (rd4.checked===true){
+pw = pw + specialChar;  
+}
+
+while (emptyString.length < pwLength) {
+  emptyString += pw[Math.floor(Math.random() * pw.length)]; 
+}
+var newPW = emptyString;
+
+
+if (pwLength.length != "" && pwLength !== "0"){
+  document.getElementById("numCatch").setAttribute("style", "color:black");
+return newPW;
+
+} else {
+document.getElementById("numCatch").setAttribute("style", "color:red");
+return "Set the number of password characters.  It cannot be blank or 0 (zero).";
+
+  // alert("Add a number");
+}
+}
+
+
+// old code - for retetion of method used
+
+ // emptyString += upperCaseAlpha[Math.floor(Math.random() * upperCaseAlpha.length)];
+  // emptyString += lowerCaseAlpha[Math.floor(Math.random() * lowerCaseAlpha.length)];
+  // emptyString += numeric[Math.floor(Math.random() * numeric.length)];
+  // emptyString += specialChar[Math.floor(Math.random() * specialChar.length)];
+
+
+// while (emptyString.length < 726) {
+//   emptyString += upperCaseAlpha[Math.floor(Math.random() * upperCaseAlpha.length)];
+//   emptyString += lowerCaseAlpha[Math.floor(Math.random() * lowerCaseAlpha.length)];
+//   emptyString += numeric[Math.floor(Math.random() * numeric.length)];
+//   emptyString += specialChar[Math.floor(Math.random() * specialChar.length)];
+// }
+// console.log(emptyString);
 
 
 // Button setup
@@ -44,23 +96,6 @@ console.log(emptyString);
 // });
 
 
-function fn1() {
 
-let result = document.getElementById("password");
-
-var rd1 = document.getElementById("option1");
-var rd2 = document.getElementById("option2");
-var rd3 = document.getElementById("option3");
-var rd4 = document.getElementById("option4");
-
-if (rd1.checked===true)  {
-result.innerText = "WHOAAAAA";
-}
-
-
-if (rd2.checked===true){
-result.innerText = "WHOAAAAA@@@@@@@@@@@@@@";
-}
-}
 
 
